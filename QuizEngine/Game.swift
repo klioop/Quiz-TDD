6f8d0c0ca41843e61df11878c7bdb7855387f822
Sam <klioop2@gmail.com>
2022-01-17 22:56:37 +0900

    scoring

--- QuizEngine/Game.swift
+++ QuizEngine/Game.swift
@@ -18,13 +18,26 @@ public class Game<Question: Hashable, Answer, R: Router> where R.Question == Que
 }
 
 // We need a strong hold onto flow, otherwise it will disappear after calling start method
-public func startGame<Question: Hashable, Answer, R: Router>(
+public func startGame<Question: Hashable, Answer: Equatable, R: Router>(
     questions: [Question],
     router: R,
     correctAnswers: [Question: Answer]
 ) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer  {
-    let flow = Flow(questions: questions, router: router, scoring: { _ in 1 } )
+    let flow = Flow(
+        questions: questions,
+        router: router,
+        scoring: { scoring($0, correctAnswers: correctAnswers) }
+    )
     flow.start()
     
     return Game(flow: flow)
+}
+
+private func scoring<Question: Hashable, Answer: Equatable>(
+    _ answers: [Question: Answer],
+    correctAnswers: [Question: Answer]
+) -> Int {
+    return answers.reduce(0) { (score, tuple) in
+        return score + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
+    }
 }
--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -12,14 +12,24 @@ class GameTest: XCTestCase {
     var game: Game<String, String, RouterSpy>!
     let router = RouterSpy()
     
-    func test_gameStart_correctOneOutOfTwo_scores1() {
-        game = startGame(questions: ["Q1, Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+    func test_gameStart_zeroCorrectOutOfTwo_scores0() {
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+        
+        router.answerCallBack("WrongAnswer")
+        router.answerCallBack("WrongAnswer")
+        
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+    
+    func test_gameStart_correctOneOutOfTwo_scoresOne() {
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
         
         router.answerCallBack("A1")
         router.answerCallBack("WrongAnswer")
         
         XCTAssertEqual(router.routedResult!.score, 1)
     }
+
 
     
 }
