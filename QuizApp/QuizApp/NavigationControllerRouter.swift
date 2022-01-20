b67e829676c0def77486e7961679a938963b3c74
Sam <klioop2@gmail.com>
2022-01-20 22:36:01 +0900

    dd

--- QuizApp/QuizApp/NavigationControllerRouter.swift
+++ QuizApp/QuizApp/NavigationControllerRouter.swift
@@ -8,17 +8,24 @@
 import UIKit
 import QuizEngine
 
+protocol ViewControllerFactory {
+    func questionViewController(for question: String, answerCallBack: @escaping (String) -> Void) -> UIViewController
+}
+
 
 class NavigationControllerRouter: Router {
     
     private let navigationController: UINavigationController
+    private let factory: ViewControllerFactory
     
-    init(navigationController: UINavigationController) {
+    init(navigationController: UINavigationController, factory: ViewControllerFactory) {
         self.navigationController = navigationController
+        self.factory = factory
     }
             
     func routeTo(question: String, answerCallBack: @escaping (String) -> Void) {
-        self.navigationController.pushViewController(UIViewController(), animated: false)
+        let viewController = factory.questionViewController(for: question, answerCallBack: answerCallBack)
+        self.navigationController.pushViewController(viewController, animated: false)
     }
     
     func routeTo(result: Result<String, String>) {
--- QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
+++ QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
@@ -13,22 +13,51 @@ import XCTest
 
 class NavigationControllerRouterTest: XCTestCase {
     
-    func test_routeToQuestion_presentsQuestionViewController() {
-        let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController: navigationController )
-        
-        sut.routeTo(question: "Q1", answerCallBack: { _ in })
-        
-        XCTAssertEqual(navigationController.viewControllers.count, 1)
-    }
+    let navigationController = UINavigationController()
+    let factory = ViewControllerFactoryStub()
+    lazy var sut: NavigationControllerRouter = {
+        return NavigationControllerRouter(navigationController: self.navigationController, factory: self.factory )
+    }()
     
-    func test_routeToQuestionTwice_presentsQuestionViewController() {
-        let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController: navigationController )
+        
+    func test_routeToQuestionTwice_showsQuestionViewController() {
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+                
+        factory.stub(question: "Q1", with: viewController)
+        factory.stub(question: "Q2", with: secondViewController)
         
         sut.routeTo(question: "Q1", answerCallBack: { _ in })
         sut.routeTo(question: "Q2", answerCallBack: { _ in })
         
         XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
+    }
+    
+    func test_routeToQuestion_presentsQuestionViewControllerWithRightCallBack() {
+        var callBackWasFired = false
+        
+        sut.routeTo(question: "Q1", answerCallBack: { _ in callBackWasFired = true })
+        factory.answerCallBack["Q1"]!("Anything")
+        
+        XCTAssertTrue(callBackWasFired)
+    }
+    
+    class ViewControllerFactoryStub: ViewControllerFactory {
+        
+        private var stubbedQuestions = [String: UIViewController]()
+        var answerCallBack = [String: (String) -> Void]()
+        
+        func stub(question: String, with viewController: UIViewController) {
+            stubbedQuestions[question] = viewController
+        }
+        
+        func questionViewController(for question: String, answerCallBack: @escaping (String) -> Void) -> UIViewController {
+            self.answerCallBack[question] = answerCallBack
+            return stubbedQuestions[question] ?? UIViewController()
+        }
+        
+        
     }
 }
